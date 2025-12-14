68fc9eb5e1b5a549f2c97d8dd653a4ccc861a931
Adam Rumpf <arumpf@floridapoly.edu>
2025-12-14 18:16:34 -0500

    Setting up initial files.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# killer-sudoku-tables
+# Killer Sudoku Tables
+
 Programs for generating tables of integer partitions for use in solving Killer Sudoku puzzles.
+
+## Introduction
+
+[Killer Sudoku](https://en.wikipedia.org/wiki/Killer_sudoku) (a.k.a. Sumdoku) is a popular Sudoku variant wherein some collections of cells are circled by cages, with the sum of the digits in the cage being given as a clue. Solving Killer Sudoku puzzles often relies on quickly [partitioning an integer](https://en.wikipedia.org/wiki/Integer_partition) (the cage sum) into a sum of a specific number of integers (the number of cells in the cage). Experts simply have common integer partitions memorized, so these scripts are meant for generating tables for quick reference.
+
+## Contents
+
+This repo includes a Python file for generating relevant partitions along with a text file listing relevant results. The file defines some functions that can be used for generating specific types of partitions or partitions falling within specific ranges, while the text file is simply a table containing a range of specific results. Since this is meant for Sudoku puzzles, only sums containing digits 1-9 are included. Lists containing no repeat digits are highlighted since these represent the only valid partitions for cages lying entirely within a single row, column, or 3x3 box.
---
+++ sudoku-partitions.py
@@ -0,0 +1,107 @@
+"""Functions for generating integer partitions relevant to Killer Sudoku."""
+
+#==============================================================================
+
+def integer_partition(n, m, d=9):
+    """Generates a list of partitions of n into m digits of max value d.
+    
+    Inputs:
+        n (int) - Integer to be partitioned.
+        m (int) - Number of digits into which to partition n.
+        d (int) - Maximum digit into which to partition n. Defaults to 9.
+    
+    Returns:
+        (list(list)) - List of lists including all possible collections of m
+            digits (values 1-d) that sum to n, in lexicographic order.
+    """
+    
+    pass
+
+#------------------------------------------------------------------------------
+
+def _repeats(lst):
+    """Determines whether a list includes repeated values.
+    
+    Inputs:
+        lst (list) - List to check.
+    
+    Returns:
+        (bool) - True if there is an element repeated in lst, False otherwise.
+    """
+    
+    pass
+
+#------------------------------------------------------------------------------
+
+def all_partitions(nmin, nmax, mmin, mmax, d=9):
+    """Generates a list of partitions over a range of digits and sizes.
+    
+    Inputs:
+        nmin, nmnax (int) - Smallest and largest digits for which to generate
+            partitions.
+        mmin, mmax (int) - Smallest and largest partition lengths (i.e. number
+            of digits) for which to generate partitions.
+        d (int) - Maximum digit in partitions. Defaults to 9.
+    
+    Returns:
+        (list(list(list))) - List of lists of lists including all partitions
+            within the specified ranges. If the elements of the list are indexed
+            as [i][j][k], then i indicates the number being partitioned, j
+            indicates the number of digits into which the number is being
+            partitioned, and k indicates the partition within that list
+            (lexicographically ordered).
+    """
+    
+    pass
+
+#------------------------------------------------------------------------------
+
+def _format_partitions(lst, repfree=False):
+    """Formats a list of partitions as a readable string.
+    
+    Inputs:
+        lst (list(list(list))) - List of lists of lists including a range of
+            partitions, of the format returned by the all_partitions function.
+        repfree (bool) - Whether to list repeat-free partitions first in their
+            own separate lines. Defaults to False.
+    
+    Returns:
+        (str) - A string listing the partitions in a readable format.
+    
+    Specifically, the string is formatted to list each total in a separate
+    paragraph. Each line of the paragraph corresponds to a different number of
+    digits into which the total can be partitioned, and each entry in the line
+    lists (in lexicographic order) all possible sums of the given length.
+    
+    If repfree is set to True, then each set of partitions of a particular sum
+    with a particular length is listed in two separate lines, first showing the
+    repeat-free partitions followed by the partitions that include repeated
+    digits.
+    """
+    
+    pass
+
+#------------------------------------------------------------------------------
+
+def print_partitions(lst, fname=None, repfree=False):
+    """Prints a list of partitions to an output file.
+    
+    Inputs:
+        lst (list(list(list))) - List of lists of lists including a range of
+            partitions, of the format returned by the all_partitions function.
+        fname (str) - Name of the output into which to print the partitions.
+            Defaults to None, in which case the string is printed to the screen.
+        repfree (bool) - Whether to list repeat-free partitions first in their
+            own separate lines. Defaults to False.
+    
+    Returns:
+        None - Returns nothing, but prints the contents of the partition list
+            into an output text file in the readable string format returned by
+            the _format_partitions function.
+    """
+    
+    pass
+
+#==============================================================================
+
+# Include scripts for generating the main table here.
